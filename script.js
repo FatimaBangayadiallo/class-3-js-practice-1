803295676c6ed6013389457170a0d112cabe68bd
BangayaDiall <bangayadiall@gmail.com>
2021-07-28 12:27:29 +0100

    the alll

---
+++ script.js
@@ -0,0 +1,19 @@
+let moi = {
+    name: 'Fatima',
+    email: 'bangayadiall@gmail.com',
+    age: 24
+
+};
+console.log(moi);
+let product = {
+    nom: 'lat',
+    pays: 'guinee',
+    prix: 20
+};
+console.log(product);
+let thinks = {
+    item1: 'food',
+    item2: 'house',
+    item3: 'rue'
+};
+console.log(thinks);
